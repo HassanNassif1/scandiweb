13fae5ab330127e7a110661427ec2b973262d446
Hasan Nassif <hassannassif.lb@gmail.com>
2024-09-11 22:27:26 +0400

    last update for the project

--- frontend/src/apollo.js
+++ frontend/src/apollo.js
@@ -2,7 +2,7 @@ import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 
 const client = new ApolloClient({
-    uri: 'https://1hassan-nassif.site/graphql',
+    uri: 'https://hassan-nassif.site/graphql',
     cache: new InMemoryCache(),
   });
   
